cdb90554fe21dc86d56b8f9b0610fbc04648f5d3
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2024-04-09 00:39:42 +0700

    wgm_conf: Add iptables commands to the wg_conf
    To allow the IP forward and bind IP features to function, we need to
    integrate such information with iptables. For forwarding without an
    explicit bind address, it'll use MASQUERADE, and for the specific
    address, it'll use SNAT.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- src/wgm_conf.c
+++ src/wgm_conf.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 static char *get_conf_path(const struct wgm_iface *iface, struct wgm_ctx *ctx)
 {
@@ -33,9 +34,70 @@ static int fprint_str_arr(FILE *h, const struct wgm_str_array *arr)
 	return ret;
 }
 
+static int wgm_conf_write_iptables(FILE *h, const struct wgm_iface *iface)
+{
+	size_t i, j;
+
+	fprintf(h, "\n");
+
+	fprintf(h, "PostUp   = iptables -t nat -F wgm_%s || true >> /dev/null 2>&1\n", iface->ifname);
+	fprintf(h, "PostUp   = iptables -t nat -N wgm_%s || true >> /dev/null 2>&1\n", iface->ifname);
+	fprintf(h, "PostUp   = iptables -t nat -I POSTROUTING -j wgm_%s || true >> /dev/null 2>&1\n", iface->ifname);
+	fprintf(h, "PostDown = iptables -t nat -D POSTROUTING -j wgm_%s\n", iface->ifname);
+	fprintf(h, "PostDown = iptables -t nat -F wgm_%s\n", iface->ifname);
+	fprintf(h, "PostDown = iptables -t nat -X wgm_%s\n", iface->ifname);
+
+	fprintf(h, "\n");
+
+	fprintf(h, "PostUp   = iptables -t filter -F wgm_%s || true >> /dev/null 2>&1\n", iface->ifname);
+	fprintf(h, "PostUp   = iptables -t filter -N wgm_%s || true >> /dev/null 2>&1\n", iface->ifname);
+	fprintf(h, "PostUp   = iptables -t filter -I FORWARD -j wgm_%s || true >> /dev/null 2>&1\n", iface->ifname);
+	fprintf(h, "PostDown = iptables -t filter -D POSTROUTING -j wgm_%s\n", iface->ifname);
+	fprintf(h, "PostDown = iptables -t filter -F wgm_%s\n", iface->ifname);
+	fprintf(h, "PostDown = iptables -t filter -X wgm_%s\n", iface->ifname);
+
+	fprintf(h, "\n");
+
+	for (i = 0; i < iface->peers.nr; i++) {
+		const struct wgm_peer *peer = &iface->peers.peers[i];
+
+		if (peer->allowed_ips.nr)
+			fprintf(h, "### Start for peer %s\n", peer->public_key);
+
+		for (j = 0; j < peer->allowed_ips.nr; j++) {
+			const char *src = peer->allowed_ips.arr[j];
+
+			fprintf(h, "PostUp   = iptables -t filter -A wgm_%s -s %s -j ACCEPT\n", iface->ifname, src);
+			if (peer->bind_ip[0])
+				fprintf(h, "PostUp   = iptables -t nat -A wgm_%s -s %s -j SNAT --to %s\n", iface->ifname, src, peer->bind_ip);
+		}
+
+		/*
+		 * Repeat the loop for any client who doesn't have a bind IP.
+		 * MASQUERADE must be placed at the end, so the loop needs to
+		 * be repeated.
+		 */
+		for (j = 0; j < peer->allowed_ips.nr; j++) {
+			const char *src = peer->allowed_ips.arr[j];
+
+			if (!peer->bind_ip[0])
+				fprintf(h, "PostUp   = iptables -t nat -A wgm_%s -s %s -j MASQUERADE\n", iface->ifname, src);
+		}
+
+		if (peer->allowed_ips.nr)
+			fprintf(h, "### End for peer %s\n", peer->public_key);
+	}
+
+	fprintf(h, "\n");
+	fprintf(h, "PostUp   = iptables -t nat -A wgm_%s -j RETURN\n", iface->ifname);
+	fprintf(h, "PostUp   = iptables -t filter -A wgm_%s -j RETURN\n", iface->ifname);
+	return 0;
+}
+
 static int wgm_conf_write(FILE *h, const struct wgm_iface *iface)
 {
 	size_t i, n;
+	int ret;
 
 	fprintf(h, "[Interface]\n");
 	fprintf(h, "ListenPort = %hu\n", iface->listen_port);
@@ -51,6 +113,9 @@ static int wgm_conf_write(FILE *h, const struct wgm_iface *iface)
 	fprintf(h, "AllowedIPs = ");
 	fprint_str_arr(h, &iface->allowed_ips);
 	fprintf(h, "\n");
+	ret = wgm_conf_write_iptables(h, iface);
+	if (ret)
+		return ret;
 
 	n = iface->peers.nr;
 	for (i = 0; i < n; i++) {
