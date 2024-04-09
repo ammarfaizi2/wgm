ammarfaizi2/wgm

commit 5209ac82fa3b130d7cf0fe419df7c66c94693857

wgm_{iface,conf}: Use wgm_asprintf and make subdirectory data
Use wgm_asprintf to simplify dynamic string generation. Also, make
subdirectory data separate from the wgm_conf for better data
directory management.

Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/wgm_conf.c
@@ -8,15 +8,25 @@
 
 static char *get_conf_path(const struct wgm_iface *iface, struct wgm_ctx *ctx)
 {
-	size_t len;
 	char *ret;
+	int err;
 
-	len = strlen(ctx->data_dir) + strlen(iface->ifname) + 32;
-	ret = malloc(len);
-	if (!ret)
+	err = wgm_asprintf(&ret, "%s/wg_conf", ctx->data_dir);
+	if (err < 0)
 		return NULL;
 
-	snprintf(ret, len, "%s/%s.conf", ctx->data_dir, iface->ifname);
+	err = mkdir_recursive(ret, 0700);
+	free(ret);
+	if (err) {
+		wgm_log_err("Failed to create directory '%s': %s\n", ret, strerror(-err));
+		free(ret);
+		return NULL;
+	}
+
+	err = wgm_asprintf(&ret, "%s/wg_conf/%s.conf", ctx->data_dir, iface->ifname);
+	if (err < 0)
+		return NULL;
+
 	return ret;
 }
 

src/wgm_iface.c
@@ -341,28 +341,26 @@ static int wgm_peer_array_copy(struct wgm_peer_array *dst, const struct wgm_peer
 	return 0;
 }
 
-static char *wgm_iface_get_file_path(struct wgm_ctx *ctx, const char *devname)
+static char *wgm_iface_get_json_path(struct wgm_ctx *ctx, const char *devname)
 {
 	char *path;
-	size_t len;
 	int ret;
 
-	len = strlen(ctx->data_dir) + strlen(devname) + 7;
-	path = malloc(len);
-	if (!path) {
-		wgm_log_err("Error: wgm_iface_get_file_path: Failed to allocate memory\n");
+	ret = wgm_asprintf(&path, "%s/json", ctx->data_dir);
+	if (ret)
 		return NULL;
-	}
 
-	ret = mkdir_recursive(ctx->data_dir, 0700);
+	ret = mkdir_recursive(path, 0700);
+	free(path);
 	if (ret) {
-		wgm_log_err("Error: wgm_iface_get_file_path: Failed to create directory '%s': %s\n",
-			    ctx->data_dir, strerror(-ret));
-		free(path);
+		wgm_log_err("Error: wgm_iface_get_json_path: Failed to create directory '%s': %s\n", path, strerror(-ret));
 		return NULL;
 	}
 
-	snprintf(path, len, "%s/%s.json", ctx->data_dir, devname);
+	ret = wgm_asprintf(&path, "%s/json/%s.json", ctx->data_dir, devname);
+	if (ret)
+		return NULL;
+
 	return path;
 }
 
@@ -637,7 +635,7 @@ int wgm_iface_load(struct wgm_iface *iface, struct wgm_ctx *ctx, const char *dev
 	FILE *fp;
 	int ret;
 
-	path = wgm_iface_get_file_path(ctx, devname);
+	path = wgm_iface_get_json_path(ctx, devname);
 	if (!path)
 		return -ENOMEM;
 
@@ -764,7 +762,7 @@ int wgm_iface_del(const struct wgm_iface *iface, struct wgm_ctx *ctx)
 	char *path;
 	int ret;
 
-	path = wgm_iface_get_file_path(ctx, iface->ifname);
+	path = wgm_iface_get_json_path(ctx, iface->ifname);
 	if (!path)
 		return -ENOMEM;
 
@@ -783,7 +781,7 @@ int wgm_iface_save(const struct wgm_iface *iface, struct wgm_ctx *ctx)
 	char *path, *jstr;
 	FILE *fp;
 
-	path = wgm_iface_get_file_path(ctx, iface->ifname);
+	path = wgm_iface_get_json_path(ctx, iface->ifname);
 	if (!path)
 		return -ENOMEM;
 

commit 67725e5b1044d0bacae2efc02b690aea183da996

wgm_{iface,conf}: Fix potential double free and memory leak
Fixes: 5209ac82fa3b130d ("wgm_{iface,conf}: Use wgm_asprintf and make subdirectory data")
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/wgm_conf.c
@@ -16,13 +16,13 @@ static char *get_conf_path(const struct wgm_iface *iface, struct wgm_ctx *ctx)
 		return NULL;
 
 	err = mkdir_recursive(ret, 0700);
-	free(ret);
 	if (err) {
 		wgm_log_err("Failed to create directory '%s': %s\n", ret, strerror(-err));
 		free(ret);
 		return NULL;
 	}
 
+	free(ret);
 	err = wgm_asprintf(&ret, "%s/wg_conf/%s.conf", ctx->data_dir, iface->ifname);
 	if (err < 0)
 		return NULL;

src/wgm_iface.c
@@ -351,12 +351,13 @@ static char *wgm_iface_get_json_path(struct wgm_ctx *ctx, const char *devname)
 		return NULL;
 
 	ret = mkdir_recursive(path, 0700);
-	free(path);
 	if (ret) {
 		wgm_log_err("Error: wgm_iface_get_json_path: Failed to create directory '%s': %s\n", path, strerror(-ret));
+		free(path);
 		return NULL;
 	}
 
+	free(path);
 	ret = wgm_asprintf(&path, "%s/json/%s.json", ctx->data_dir, devname);
 	if (ret)
 		return NULL;