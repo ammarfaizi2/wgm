a74fc0042dff2dc419887213b29a3d2eca9485be
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2024-04-09 21:45:10 +0700

    wgm_conf: Create fwmark tracking
    Implement multiple routing using fwmark, each client can have its own
    route.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- src/wgm_conf.c
+++ src/wgm_conf.c
@@ -5,6 +5,121 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <sys/file.h>
+
+static int get_fwmark_path(char **out, const char *src, const char *ip,
+			   struct wgm_ctx *ctx)
+{
+	char *ret;
+	int err;
+
+	err = wgm_asprintf(&ret, "%s/fwmark", ctx->data_dir);
+	if (err)
+		return err;
+
+	err = mkdir_recursive(ret, 0700);
+	if (err) {
+		wgm_log_err("Failed to create directory '%s': %s\n", ret, strerror(-err));
+		free(ret);
+		return err;
+	}
+
+	free(ret);
+	err = wgm_asprintf(&ret, "%s/fwmark/%s-%s.txt", ctx->data_dir, src, ip);
+	if (err)
+		return err;
+
+	*out = ret;
+	return 0;
+}
+
+static int generate_fwmark_auto_increment(unsigned *mark, struct wgm_ctx *ctx)
+{
+	unsigned tmp;
+	char *fpath;
+	FILE *fp;
+	int ret;
+
+	ret = wgm_asprintf(&fpath, "%s/fwmark.last", ctx->data_dir);
+	if (ret)
+		return ret;
+
+	fp = fopen(fpath, "rb+");
+	if (!fp) {
+		fp = fopen(fpath, "wb");
+		if (!fp) {
+			ret = -errno;
+			wgm_log_err("Failed to create fwmark file '%s': %s\n", fpath, strerror(-ret));
+			return ret;
+		}
+
+		flock(fileno(fp), LOCK_EX);
+		fprintf(fp, "%u\n", 37000u);
+		fclose(fp);
+		*mark = 37000u;
+		return 0;
+	}
+
+	flock(fileno(fp), LOCK_EX);
+	if (fscanf(fp, "%u\n", &tmp) != 1) {
+		wgm_log_err("Failed to read fwmark file '%s': Invalid unsigned integer format\n", fpath);
+		fclose(fp);
+		return -EINVAL;
+	}
+	rewind(fp);
+	fprintf(fp, "%u\n", tmp + 1);
+	fclose(fp);
+	*mark = tmp;
+	return 0;
+}
+
+static int get_fwmark(unsigned *mark, const char *src, const char *ip,
+		      struct wgm_ctx *ctx)
+{
+	char *fpath;
+	FILE *fp;
+	int ret;
+
+	ret = get_fwmark_path(&fpath, src, ip, ctx);
+	if (ret)
+		return ret;
+
+	fp = fopen(fpath, "rb+");
+	if (!fp) {
+		unsigned tmp_mark;
+
+		fp = fopen(fpath, "wb");
+		if (!fp) {
+			ret = -errno;
+			wgm_log_err("Failed to create fwmark file '%s': %s\n", fpath, strerror(-ret));
+			goto out;
+		}
+
+		flock(fileno(fp), LOCK_EX);
+		ret = generate_fwmark_auto_increment(&tmp_mark, ctx);
+		if (ret)
+			goto out;
+
+		ret = 0;
+		*mark = tmp_mark;
+		fprintf(fp, "%u\n", tmp_mark);
+		goto out;
+	}
+
+	flock(fileno(fp), LOCK_EX);
+	if (fscanf(fp, "%u\n", mark) != 1) {
+		wgm_log_err("Failed to read fwmark file '%s': Invalid unsigned integer format\n", fpath);
+		ret = -EINVAL;
+	}
+
+out:
+	fclose(fp);
+	if (ret)
+		remove(fpath);
+
+	free(fpath);
+	return ret;
+}
 
 static char *get_conf_path(const struct wgm_iface *iface, struct wgm_ctx *ctx)
 {
@@ -44,7 +159,8 @@ static int fprint_str_arr(FILE *h, const struct wgm_str_array *arr)
 	return ret;
 }
 
-static int wgm_conf_write_iptables(FILE *h, const struct wgm_iface *iface)
+static int wgm_conf_write_iptables(FILE *h, const struct wgm_iface *iface,
+				   struct wgm_ctx *ctx)
 {
 	size_t i, j;
 
@@ -62,24 +178,39 @@ static int wgm_conf_write_iptables(FILE *h, const struct wgm_iface *iface)
 	fprintf(h, "PostUp   = iptables -t filter -F wgm_%s || true >> /dev/null 2>&1\n", iface->ifname);
 	fprintf(h, "PostUp   = iptables -t filter -N wgm_%s || true >> /dev/null 2>&1\n", iface->ifname);
 	fprintf(h, "PostUp   = iptables -t filter -I FORWARD -j wgm_%s || true >> /dev/null 2>&1\n", iface->ifname);
-	fprintf(h, "PostDown = iptables -t filter -D POSTROUTING -j wgm_%s\n", iface->ifname);
 	fprintf(h, "PostDown = iptables -t filter -F wgm_%s\n", iface->ifname);
 	fprintf(h, "PostDown = iptables -t filter -X wgm_%s\n", iface->ifname);
 
 	fprintf(h, "\n");
 
+	fprintf(h, "PostUp   = iptables -t mangle -F wgm_%s || true >> /dev/null 2>&1\n", iface->ifname);
+	fprintf(h, "PostUp   = iptables -t mangle -N wgm_%s || true >> /dev/null 2>&1\n", iface->ifname);
+	fprintf(h, "PostDown = iptables -t mangle -D PREROUTING -j wgm_%s\n", iface->ifname);
+	fprintf(h, "PostDown = iptables -t mangle -F wgm_%s\n", iface->ifname);
+	fprintf(h, "PostDown = iptables -t mangle -X wgm_%s\n", iface->ifname);
+
 	for (i = 0; i < iface->peers.nr; i++) {
 		const struct wgm_peer *peer = &iface->peers.peers[i];
 
 		if (peer->allowed_ips.nr)
-			fprintf(h, "### Start for peer %s\n", peer->public_key);
+			fprintf(h, "\n### Start for peer %s\n", peer->public_key);
 
 		for (j = 0; j < peer->allowed_ips.nr; j++) {
 			const char *src = peer->allowed_ips.arr[j];
+			unsigned mark;
+			int ret;
 
 			fprintf(h, "PostUp   = iptables -t filter -A wgm_%s -s %s -j ACCEPT\n", iface->ifname, src);
-			if (peer->bind_ip[0])
+			if (peer->bind_ip[0]) {
+				ret = get_fwmark(&mark, peer->bind_ip, peer->bind_dev, ctx);
+				if (ret)
+					return ret;
+				fprintf(h, "PostUp   = iptables -t mangle -A wgm_%s -s %s -j MARK --set-mark %u\n", iface->ifname, src, mark);
 				fprintf(h, "PostUp   = iptables -t nat -A wgm_%s -s %s -j SNAT --to %s\n", iface->ifname, src, peer->bind_ip);
+				fprintf(h, "PostUp   = ip rule add fwmark %u lookup %u\n", mark, mark);
+				fprintf(h, "PostDown = ip rule del fwmark %u lookup %u\n", mark, mark);
+				fprintf(h, "PostUp   = ip route replace default dev %s table %u\n", peer->bind_dev, mark);
+			}
 		}
 
 		/*
@@ -101,10 +232,12 @@ static int wgm_conf_write_iptables(FILE *h, const struct wgm_iface *iface)
 	fprintf(h, "\n");
 	fprintf(h, "PostUp   = iptables -t nat -A wgm_%s -j RETURN\n", iface->ifname);
 	fprintf(h, "PostUp   = iptables -t filter -A wgm_%s -j RETURN\n", iface->ifname);
+	fprintf(h, "PostUp   = iptables -t mangle -A wgm_%s -j RETURN\n", iface->ifname);
 	return 0;
 }
 
-static int wgm_conf_write(FILE *h, const struct wgm_iface *iface)
+static int wgm_conf_write(FILE *h, const struct wgm_iface *iface,
+			  struct wgm_ctx *ctx)
 {
 	size_t i, n;
 	int ret;
@@ -120,10 +253,7 @@ static int wgm_conf_write(FILE *h, const struct wgm_iface *iface)
 			fprintf(h, ",");
 	}
 	fprintf(h, "\n");
-	fprintf(h, "AllowedIPs = ");
-	fprint_str_arr(h, &iface->allowed_ips);
-	fprintf(h, "\n");
-	ret = wgm_conf_write_iptables(h, iface);
+	ret = wgm_conf_write_iptables(h, iface, ctx);
 	if (ret)
 		return ret;
 
@@ -160,7 +290,7 @@ int wgm_conf_save(const struct wgm_iface *iface, struct wgm_ctx *ctx)
 		return -ENOMEM;
 	}
 
-	ret = wgm_conf_write(fp, iface);
+	ret = wgm_conf_write(fp, iface, ctx);
 	free(path);
 	return ret;
 }
