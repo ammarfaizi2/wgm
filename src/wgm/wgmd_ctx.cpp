3193aeedeb46299e3fe7be908af44869c84af00d
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2024-05-25 10:26:08 +0700

    clean up
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- src/wgm/wgmd_ctx.cpp
+++ src/wgm/wgmd_ctx.cpp
@@ -9,17 +9,16 @@ namespace wgm {
 wgmd_ctx::wgmd_ctx(const std::string &config_file_path,
 		   const std::string &client_cfg_dir):
 	config_file_path_(config_file_path),
-	client_cfg_dir_(client_cfg_dir)
+	client_cfg_dir_(client_cfg_dir),
+	config_file_(std::make_unique<file_t>(config_file_path_))
 {
 	load_config();
 }
 
 void wgmd_ctx::load_config(void)
 {
-	size_t i;
-
-	config_file_ = std::make_unique<file_t>(config_file_path_);
 	json servers = config_file_->get_json();
+	size_t i;
 
 	for (i = 0; i < servers.size(); i++) {
 		try {
@@ -28,8 +27,7 @@ void wgmd_ctx::load_config(void)
 
 			servers_.emplace(cfg.location, cfg);
 		} catch (const std::exception &e) {
-			pr_warn("Failed to parse server config at index %zu: %s, skipping...\n",
-				i, e.what());
+			pr_warn("Failed to parse server config at index %zu: %s, skipping...\n", i, e.what());
 		}
 	}
 }
